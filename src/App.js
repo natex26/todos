2cc6d2aea7e0e7c5793fdcbf313eebac95606fa2
natex26 <ibaishwanadoo@gmail.com>
2023-03-20 12:40:34 +0100

    add hello world

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to our first todo app</p>
-        
+        <h1>Hello world</h1>
       </header>
     </div>
   );
